4ff86f2a8b9afc5846f691ac28eb66ef33047517
Hans Trautlein <hans@trautlein.com>
2016-09-26 14:40:08 -0700

    Instead of using $.ajax changed to $.get + $.post

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -12,47 +12,18 @@ var message = {
   username: 'TESTtestTESTtestTEST',
   text: 'TESTING 123 TESTING 123',
   roomname: 'other'
-};
-
- 
-$(".send").click(function() {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-});
-
-// should get all the data
-$.ajax({
-  url: retrieveURL,
-  type: 'GET',
-  dataType: 'json',
-  success: function (data) {
-    console.log(data);
-  }
-});
+}; 
 
 // document must be loaded completely before manipulating the DOM
 $(document).ready( function () {
   $(".send").click( function () {
-    console.log('clicked');
-
     $.ajax({
-      url: 'https://api.parse.com/1/classes/messages',
+      url: mainURL,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
-        console.log(JSON.stringify(message));
+        console.log('chatterbox: Message sent \n\n' + JSON.stringify(message));
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
@@ -63,16 +34,10 @@ $(document).ready( function () {
 
 
   $('.retrieve').click( function () {
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/messages?order=-createdAt',
-      type: 'GET',
-      dataType: 'json',
-      success: function (data) {
-        console.log(data.results);
-      }
+    $.get(retrieveURL, function(data) {
+      console.log(data);
     });
   });
 });
 
 // should post the data on click of item
-
