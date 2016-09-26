ea7a5320fb10dff96ecbed9dfc64a2bdaaecc457
trautlein <hans@trautlein.com>
2016-09-26 16:13:19 -0700

    We have passed all of the tests the 'events'

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,34 +1,72 @@
-// Example of message format:
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
-// 
-var mainURL = 'https://api.parse.com/1/classes/messages';
-var retrieveURL = mainURL + "?order=-createdAt";
+var app = {
 
-var message = {
-  username: 'HANS',
-  text: 'TESTING 123 TESTING 123',
-  roomname: 'coolkids'
-}; 
+  mainURL: 'https://api.parse.com/1/classes/messages',
+  server: 'https://api.parse.com/1/classes/messages?order=-createdAt',
+
+  testMessage: {
+    username: 'Test123',
+    text: 'This is a test',
+    roomname: 'lobby'
+  },
+
+
+  init: function () {
+
+  },
+
+  send: function (message) { 
+
+    $.ajax({
+      url: this.mainURL,
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(message),
+      success: function (data) {
+        console.log('chatterbox: Message sent \n\n' + JSON.stringify(message));
+      }
+    });
+   },
+
+  fetch: function () {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      // dataType: 'json',
+      // data: {param1: 'value1'},
+      success: function (data) {
+        for (var i = 0; i < data.results.length; i++) {
+          app.renderMessage(data.results[i]);
+        }
+      }
+    });
+  },
+  
+  clearMessages: function() {
+    $('#chats').html('');
+  }, 
+
+  renderMessage: function(message) {
+    $('#chats').append('<div>' + message.username + ": " + 
+                        message.text + '</div>');
+  }, 
+
+  renderRoom: function(room) {
+    $('#roomSelect').append('<a class="' + room + '"></a>');
+  }
+
+};
+
+
 
 // document must be loaded completely before manipulating the DOM
 $(document).ready( function () {
 
   $('.send').click( function () {
-    $.post(mainURL, JSON.stringify(message), function(data) {
-      console.log('chatterbox: Message sent \n\n' + JSON.stringify(message));
-    });
+    app.send(app.testMessage);
   });
 
 
   $('.retrieve').click( function () {
-    $.get(retrieveURL, function(data) {
-      console.log(data);
-    });
+    app.fetch();
   });
 });
-
-// should post the data on click of item
